9fac222bcb09dffda7883fb5c86818519af3cc3c
ZIYOCHIRO <ziyochiro@gmail.com>
2018-08-19 09:27:01 +0800

    changes: 1. show image with animation 2. add MARKs 3. add life cycle

---
+++ Roll the Dice/DiceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DiceViewController.swift
+//  Roll the Dice
+//
+//  Created by 10.12 on 2018/7/23.
+//  Copyright © 2018 10.12. All rights reserved.
+//
+
+import UIKit
+
+class DiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
